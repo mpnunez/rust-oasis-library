1c9c618e279fb920a59f290627ec686eca7a3a2c
Marcel Nunez <marcelpnunez@gmail.com>
2024-12-25 09:56:21 -0800

    Implementing int -> byte vector
    for write_uns_int

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,58 @@
 // Disk io
 use std::fs::File;
 use std::io::prelude::*;
-use std::fs;
 use std::io::BufWriter;
 
 struct OasisBytes {}
 
 impl OasisBytes {
-    const MAGIC_BYTES: &str = "%SEMI-OASIS\r\n";
-    const CURVI_MAGIC_BYTES: &str = "%SEMI-OASIS-CURVILINEAR\r\n";
+    const MAGIC_BYTES: &'static str = "%SEMI-OASIS\r\n";
+    const CURVI_MAGIC_BYTES: &'static str = "%SEMI-OASIS-CURVILINEAR\r\n";
 }
 
 struct RecordType {}
+
 
 impl RecordType {
     const PAD: u8 = 0;
     const START: u8 = 1;
     const END: u8 = 2;
+    const CELLNAME_IMPL_REF_NUM: u8 = 3;
+    const CELLNAME_EXPL_REF_NUM: u8 = 4;
+    const TEXT_STRING_IMPL_REF_NUM: u8 = 5;
+    const TEXT_STRING_EXPL_REF_NUM: u8 = 6;
+    const PROPNAME_STRING_IMPL_REF_NUM: u8 = 7;
+    const PROPNAME_STRING_EXPL_REF_NUM: u8 = 8;
+    const PROPSTRING_IMPL_REF_NUM: u8 = 9;
+    const PROPSTRING_EXPL_REF_NUM: u8 = 10;
+    /*
+    const : u8 = 11;
+    const : u8 = 12;
+    const : u8 = 13;
+    const : u8 = 14;
+    const : u8 = 15;
+    const : u8 = 16;
+    const : u8 = 17;
+    const : u8 = 18;
+    const : u8 = 19;
+    const RECTANGLE: u8 = 20;
+    const : u8 = 21;
+    const : u8 = 22;
+    const : u8 = 23;
+    const : u8 = 24;
+    const : u8 = 25;
+    const : u8 = 26;
+    const : u8 = 27;
+    const : u8 = 28;
+    const : u8 = 29;
+    const : u8 = 30;
+    const : u8 = 31;
+    const : u8 = 32;
+    const : u8 = 33;
+    const : u8 = 34;
+    const MULTIGON: u8 = 35;
+    */
+
 }
 
 fn read_oasis_file(fname: &str) -> std::io::Result<()> {
@@ -28,15 +64,22 @@ fn read_oasis_file(fname: &str) -> std::io::Result<()> {
     Ok(())
 }
 
+fn int_to_byte_array(n: impl num::integer::Integer) -> Vec<u8> {
+    let x: u8 = 2;
+    (&[x]).to_vec()
+}
+
 fn main() -> std::io::Result<()> {
 
-    let mut f = File::create("test.oas")?;
+    let f = File::create("test.oas")?;
     let mut bw = BufWriter::new(f);
 
     bw.write_all(OasisBytes::MAGIC_BYTES.as_bytes())?;
     bw.write_all(&[RecordType::START])?;
     bw.write_all(&[RecordType::END])?;
     bw.flush()?;    // How often do we need to flush?
+
+    int_to_byte_array(55);
 
     read_oasis_file("test.oas")?;
 
