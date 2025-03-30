82c50f83aee91aa698d479752da59707875da3be
Marcel Nunez <marcelpnunez@gmail.com>
2025-03-30 14:04:31 -0700

    dunno

--- src/geometry.rs
+++ src/geometry.rs
@@ -6,6 +6,14 @@ trait PointTrait {
     fn set_y(&mut self, _: Self::CoordinateType) -> ();
 }
 
+trait RectangleTrait {
+    type CoordinateType;
+    fn length(&self) -> Self::CoordinateType;
+    fn width(&self) -> Self::CoordinateType;
+    fn llx(&self) -> Self::CoordinateType;
+    fn lly(&self) -> Self::CoordinateType;
+}
+
 struct Point {
     x: i64,
     y: i64,
@@ -19,9 +27,27 @@ impl PointTrait for Point {
     fn set_y(&mut self, y: Self::CoordinateType) -> () {self.y = y}
 }
 
+
+
 struct Rectangle<PointType: PointTrait> {
     pt1: PointType,
     pt2: PointType,
+}
+
+impl<PointType: geometry::PointTrait> RectangleTrait for Rectangle<PointType impl geometry::PointTrait> {
+    type CoordinateType = PointType::CoordinateType;
+    fn length(&self) -> Self::CoordinateType {
+        return 0;
+    }
+    fn width(&self) -> Self::CoordinateType {
+        return 0;
+    }
+    fn llx(&self) -> Self::CoordinateType {
+        return 0;
+    }
+    fn lly(&self) -> Self::CoordinateType {
+        return 0;
+    }
 }
 
 #[cfg(test)]
@@ -30,9 +56,20 @@ mod tests {
 
     #[test]
     fn make_point(){
-        let p = Point { x: 0, y: 0 };
-        assert_eq!(p.x(),0);
-        assert_eq!(p.y(),0);
+        let p = Point { x: 4, y: 5 };
+        assert_eq!(p.x(),4);
+        assert_eq!(p.y(),5);
     }
+
+    /*
+    #[test]
+    fn make_rectangle(){
+        let pt1 = Point { x: 0, y: 0 };
+        let pt2 = Point { x: 4, y: 5 };
+        let r = Rectangle {pt1: pt1, pt2: pt2};
+        assert_eq!(r.length(),4);
+        assert_eq!(r.width(),5);
+    }
+    */
 
 }
