a0e0c357a0f0ae08742fc4725f04e6da5da67c73
Marcel Nunez <marcelpnunez@gmail.com>
2025-04-23 18:32:35 +0800

    Create struct for writing oasis file

--- src/main.rs
+++ src/main.rs
@@ -23,72 +23,106 @@ fn read_oasis_file(fname: &str) -> std::io::Result<()> {
 }
 
 
+struct OasisRecordWriter<Wot: WriteOasis> {
+    bw: Wot,
+    precision: f32,
+    oasis_type: OasisType,
+    byte_ind: usize,
+    next_cell_refnum: u64,
+}
+
+impl <Wot: WriteOasis> OasisRecordWriter<Wot> {
+
+    pub fn new(ow: Wot, p: f32) -> Self {
+        Self { 
+            bw: ow,
+            precision: p,
+            oasis_type: OasisType::STANDARD,
+            byte_ind: 0,
+            next_cell_refnum: 0,
+        }
+    }
+
+    pub fn initialize_file(&mut self) -> std::io::Result<()> {
+        self.byte_ind += self.bw.write_magic_bytes(&self.oasis_type)?;
+        self.byte_ind += self.bw.write_start_record(&self.precision)?;
+        Ok(())
+    }
+
+    pub fn write_cell_record(&mut self, name: &str) -> std::io::Result<()> {
+        self.byte_ind += self.bw.write_uns_int(RecordType::CELL_BY_REFNUM)?;
+        self.byte_ind += self.bw.write_uns_int(self.next_cell_refnum)?;
+        self.next_cell_refnum+=1;
+        // Need to store the cell name
+        Ok(())
+    }
+
+    pub fn write_rectangle_record(&mut self) -> std::io::Result<()> {
+        self.byte_ind += self.bw.write_uns_int(RecordType::RECTANGLE)?;
+        self.byte_ind += self.bw.write_info_byte( // SWHXYRDL
+            false, true, true, true, true, false, true, true
+        )?;
+        self.byte_ind += self.bw.write_uns_int(1)?;
+        self.byte_ind += self.bw.write_uns_int(0)?;
+        self.byte_ind += self.bw.write_uns_int(100)?;
+        self.byte_ind += self.bw.write_uns_int(200)?;
+        self.byte_ind += self.bw.write_sgn_int(0)?;
+        self.byte_ind += self.bw.write_sgn_int(0)?;
+        Ok(())
+    }
+
+    pub fn close_file(&mut self) -> std::io::Result<()> {
+        self.write_cell_names()?;
+        self.write_end_record()?;
+        Ok(())
+    }
+
+    fn write_cell_names(&mut self) -> std::io::Result<()> {
+        cellname_table_offset = self.byte_ind;  // TODO: write this in offset table
+        self.byte_ind += self.bw.write_uns_int(RecordType::CELLNAME_IMPL_REF_NUM)?;
+        self.byte_ind += self.bw.write_string("topcell",StringType::N)?;
+        self.byte_ind += self.bw.write_uns_int(RecordType::CELLNAME_IMPL_REF_NUM)?;
+        self.byte_ind += self.bw.write_string("cell2",StringType::N)?;
+        self.byte_ind += self.bw.write_uns_int(RecordType::CELLNAME_IMPL_REF_NUM)?;
+        self.byte_ind += self.bw.write_string("cell3",StringType::N)?;
+        Ok(())
+    }
+
+    fn write_end_record(&mut self) -> std::io::Result<()> {
+        // End record
+        self.byte_ind += self.bw.write_uns_int(RecordType::END)?;
+        let offset_table: [u8;12] = [0;12];
+        self.bw.write_all(&offset_table)?;    // non-strict table offsets
+        self.byte_ind += 12; // whatever the size of the offset table is
+        let n_bytes_other_end_stuff: usize = 12 + 1 + 1; // offset table + end validation + length of validation pad
+        // and validation
+        let n_bytes_padding: usize = OasisBytes::END_RECORD_LENGTH - n_bytes_other_end_stuff;
+        let validation_pad: Vec<u8> = vec![RecordType::PAD; n_bytes_padding];
+        self.byte_ind += self.bw.write_string(std::str::from_utf8(&validation_pad).unwrap(), StringType::B)?;
+        self.byte_ind += self.bw.write_uns_int(OasisBytes::END_RECORD_VALIDATION_NONE)?;
+        self.bw.flush()?;
+        Ok(())
+    }
+
+}
 
 fn main() -> std::io::Result<()> {
 
     let file_name = "test.oas";
-    let oasis_type = OasisType::STANDARD;
-    let precision: f32 = 8000_f32;
-
     let f = File::create(file_name)?;
     let mut bw = BufWriter::new(f);
-    let mut byte_ind: usize = 0;
 
-    byte_ind += bw.write_magic_bytes(&oasis_type)?;
-    byte_ind += bw.write_start_record(&precision)?;
-    
-    let mut next_cell_refnum: u64 = 0;
-    
-    byte_ind += bw.write_uns_int(RecordType::CELL_BY_REFNUM)?;
-    byte_ind += bw.write_uns_int(next_cell_refnum)?;
-    next_cell_refnum+=1;
+    let mut orw = OasisRecordWriter::new(bw, 8000_f32);
+    orw.initialize_file()?;
+    orw.write_cell_record("topcell")?;
+    orw.write_rectangle_record()?;   // TODO: take a Rectangle as argument
+    orw.write_cell_record("cell2")?;
+    orw.write_cell_record("cell3")?;
+    orw.close_file()?;
 
-    // write a rectangle
-    byte_ind += bw.write_uns_int(RecordType::RECTANGLE)?;
-    byte_ind += bw.write_info_byte( // SWHXYRDL
-        false, true, true, true, true, false, true, true
-    )?;
-    byte_ind += bw.write_uns_int(1)?;
-    byte_ind += bw.write_uns_int(0)?;
-    byte_ind += bw.write_uns_int(100)?;
-    byte_ind += bw.write_uns_int(200)?;
-    byte_ind += bw.write_sgn_int(0)?;
-    byte_ind += bw.write_sgn_int(0)?;
+    println!("{} bytes written.", orw.byte_ind);
 
-    byte_ind += bw.write_uns_int(RecordType::CELL_BY_REFNUM)?;
-    byte_ind += bw.write_uns_int(next_cell_refnum)?;
-    next_cell_refnum+=1;
-
-    byte_ind += bw.write_uns_int(RecordType::CELL_BY_REFNUM)?;
-    byte_ind += bw.write_uns_int(next_cell_refnum)?;
-    next_cell_refnum+=1;
-
-    let cellname_table_offset = byte_ind;
-    byte_ind += bw.write_uns_int(RecordType::CELLNAME_IMPL_REF_NUM)?;
-    byte_ind += bw.write_string("topcell",StringType::N)?;
-    byte_ind += bw.write_uns_int(RecordType::CELLNAME_IMPL_REF_NUM)?;
-    byte_ind += bw.write_string("cell2",StringType::N)?;
-    byte_ind += bw.write_uns_int(RecordType::CELLNAME_IMPL_REF_NUM)?;
-    byte_ind += bw.write_string("cell3",StringType::N)?;
-
-    
-
-    // End record
-    byte_ind += bw.write_uns_int(RecordType::END)?;
-    let offset_table: [u8;12] = [0;12];
-    bw.write_all(&offset_table)?;    // non-strict table offsets
-    byte_ind += 12; // whatever the size of the offset table is
-    let n_bytes_other_end_stuff: usize = 12 + 1 + 1; // offset table + end validation + length of validation pad
-    // and validation
-    let n_bytes_padding: usize = OasisBytes::END_RECORD_LENGTH - n_bytes_other_end_stuff;
-    let validation_pad: Vec<u8> = vec![RecordType::PAD; n_bytes_padding];
-    byte_ind += bw.write_string(std::str::from_utf8(&validation_pad).unwrap(), StringType::B)?;
-    byte_ind += bw.write_uns_int(OasisBytes::END_RECORD_VALIDATION_NONE)?;
-    bw.flush()?;
-
-    println!("{} bytes written.", byte_ind);
-
-    read_oasis_file("test.oas")?;
+    read_oasis_file(file_name)?;
 
     Ok(())
 }
